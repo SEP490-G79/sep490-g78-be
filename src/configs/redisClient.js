1dd1ceca64566c0256061325baddfa276561f54a
Nguyễn Minh Trí <133070397+TriNM2003@users.noreply.github.com>
2025-08-19 16:43:03 +0700

    Update redisClient.js

--- src/configs/redisClient.js
+++ src/configs/redisClient.js
@@ -1,16 +1,16 @@
 const Redis = require("ioredis");
 
 // cloud
-// const redisClient = new Redis(process.env.REDIS_CONNECTIONSTRING);
-// redisClient.on("connect", () => {
-//     console.log("Kết nối thành công đến Redis Cloud");
-// });
+const redisClient = new Redis(process.env.REDIS_CONNECTIONSTRING);
+redisClient.on("connect", () => {
+    console.log("Kết nối thành công đến Redis Cloud");
+});
 
 // local
-const redisClient = new Redis({
-    host: process.env.REDIS_HOST,
-    port: process.env.REDIS_PORT
-});
+// const redisClient = new Redis({
+//     host: process.env.REDIS_HOST,
+//     port: process.env.REDIS_PORT
+// });
 redisClient.on("connect", () => {
     console.log("Kết nối thành công đến Redis Local");
 });
